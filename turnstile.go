edba21d8119271c27e3e752b7b52e67cc3eb92ad
Justyn Hunter <cjhunter@me.com>
2024-09-26 22:01:44 -0500

    initial commit - full lib

---
+++ turnstile.go
@@ -0,0 +1,75 @@
+package turnstile
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// a formSubmission is the data submitted by the form
+type formSubmission struct {
+	TurnstileResponse string `json:"cf-turnstile-response"`
+}
+
+// a turnstilePostBody is the data sent to tunstile as a POST
+type turnstilePostBody struct {
+	IP       string `json:"remoteip"`
+	Response string `json:"response"`
+	Secret   string `json:"secret"`
+}
+
+// a turnstileVerifyResponseBody is the data returned from tunstile
+type turnstileVerifyResponseBody struct {
+	Success bool `json:"success"`
+}
+
+func IsSubmitterHuman(req *http.Request, turnstileUrl, turnstileSecretKey string) (bool, error) {
+	// get turnstile key from form body
+	formBody := formSubmission{}
+	if err := json.NewDecoder(req.Body).Decode(&formBody); err != nil {
+		return false, err
+	}
+
+	// get ip from headers
+	ip := req.Header.Get("CF-Connecting-IP")
+
+	// build request body
+	postBody := turnstilePostBody{
+		IP:       ip,
+		Response: formBody.TurnstileResponse,
+		Secret:   turnstileSecretKey,
+	}
+	postBytes, err := json.Marshal(postBody)
+	if err != nil {
+		log.Printf("error: %v", err.Error())
+		return false, err
+	}
+
+	// build request
+	turnstileRequest, err := http.NewRequest("POST", turnstileUrl, bytes.NewReader(postBytes))
+	if err != nil {
+		log.Printf("error: %v", err.Error())
+		return false, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	// send request
+	client := &http.Client{}
+	resp, err := client.Do(turnstileRequest)
+	if err != nil {
+		log.Printf("error: %v", err.Error())
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	// parse response
+	var response turnstileVerifyResponseBody
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil || !response.Success {
+		log.Printf("error: %v", err.Error())
+		return false, err
+	}
+
+	return true, nil
+}
